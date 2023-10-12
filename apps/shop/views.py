725c1753abf98cd238e8514c27154fd48bf5c7c0
gReen7aRRow <f.gusarov@inbox.ru>
2023-10-12 16:54:07 +0300

    moderate shoplist for order monsters by price

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,9 +1,14 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic.list import ListView
-from ..monster import models
+from ..monster.models import Monster
 
 
 class ShopListView(ListView):
 
-    model = models.Monster
+    model = Monster.objects.order_by('price')
     template_name = 'shop/shop.html'
     context_object_name = 'monsters'
+
+    def get_queryset(self):
+        return Monster.objects.order_by('price')
