72faa9a3482d138792a53df87252c4f7cdd5c855
gReen7aRRow <f.gusarov@inbox.ru>
2023-10-12 16:49:36 +0300

    create all monsters im db; create utils in root dir; remake user model for count monsters;

--- apps/monster/models.py
+++ apps/monster/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Monster(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    description = models.TextField(max_length=200)
-    image = models.ImageField(upload_to='images/monsters/', default=None)
+    description = models.TextField(max_length=200, blank=True)
+    image = models.ImageField(upload_to='images/monsters/', blank=True)
     price = models.IntegerField()
     productivity = models.IntegerField()
 
---
+++ django_app/utils.py
@@ -0,0 +1,11 @@
+"""Module with any utils for project"""
+
+def get_default_dict():
+    """return a empty dict for count users monsters"""
+
+    return {
+        "red_monster": 0,
+        "yellow_monster": 0,
+        "blue_monster": 0,
+        "black_monster": 0,
+    }
