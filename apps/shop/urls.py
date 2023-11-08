e7cd8861d9f2251b6d4ed9af2b6c1b6a75801980
gReen7aRRow <f.gusarov@inbox.ru>
2023-11-08 11:52:47 +0300

    create buy_monster logic

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('shop/', views.ShopListView.as_view(), name='shop'),
+    path('shop/buy/<int:pk>/', views.buy_monster, name='buy-monster'),
 ]
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.list import ListView
-from ..monster.models import Monster
+from ..monster.models import Monster, MonsterCounter
+from django.shortcuts import get_object_or_404, redirect
+
 
 
 class ShopListView(ListView):
@@ -10,3 +12,23 @@ class ShopListView(ListView):
 
     def get_queryset(self):
         return Monster.objects.order_by('price')
+    
+
+def buy_monster(request, pk):
+    monster = get_object_or_404(Monster, pk=pk)
+    quantity = int(request.POST['quantity'])
+    summ = monster.price * quantity
+
+    if request.user.profile.balance >= summ:
+        counter = MonsterCounter.objects.get(owner=request.user, monster=pk)
+
+        request.user.profile.balance -= summ
+        counter.quantity += quantity
+
+        request.user.save()
+        counter.save()        
+
+        return redirect('profile')
+    
+    else:
+        return redirect('shop')
