a9eed1e06069961d2f64fac9de825ff2aa5977a9
gReen7aRRow <f.gusarov@inbox.ru>
2023-11-08 15:53:01 +0300

    create custom handler404

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,17 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class IndexView(TemplateView):
 
     template_name = 'main/index.html'
+
+
+def page_not_found_view(
+        request,
+        exception,
+        template_name='errors/404.html'):
+    
+    return render(request,
+                  template_name,
+                  status=404)
--- django_app/urls.py
+++ django_app/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+handler404 = 'apps.main.views.page_not_found_view'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
