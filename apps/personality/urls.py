5861c09b75e4d809dc0540c997cc3aad9e7246a1
gReen7aRRow <f.gusarov@inbox.ru>
2023-11-10 21:10:02 +0300

    created collect coins logic

--- apps/personality/urls.py
+++ apps/personality/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.user_profile, name='profile'),
+    path('profile/collect/<int:pk>',
+         views.collect_coins,
+         name='collect-coins'
+         ),
 ]
--- apps/personality/views.py
+++ apps/personality/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from ..monster.models import MonsterCounter
 
 
@@ -15,3 +15,18 @@ def user_profile(request):
     }
 
     return render(request, 'personality/profile.html', context=context)
+
+def collect_coins(request, pk):
+    user = request.user
+    monster = (MonsterCounter
+              .objects
+              .get(owner=user, monster=pk)
+              )
+    
+    user.profile.balance += monster.wealth
+    monster.wealth = 0
+
+    user.save()
+    monster.save()
+    
+    return redirect('profile')
